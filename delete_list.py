f6331536f3d9f0122f9f7f14a87a1248750e60ac
Spencer Wood <swood@qualtrics.com>
2016-01-26 11:36:27 -0700

    Make python 3 compatible

--- delete_list.py
+++ delete_list.py
@@ -5,8 +5,8 @@ import requests
 @click.command()
 @click.option('--user', prompt='User', help='Target audience user.')
 @click.option('--token', prompt='API Token', help='Target Audience API token.')
-@click.option('--library_id', prompt='Library ID', help='User\'s Library ID.')
-@click.option('--list_id', prompt='List ID to delete', help='ID of list to delete.')
+@click.option('--library-id', prompt='Library ID', help='User\'s Library ID.')
+@click.option('--list-id', prompt='List ID to delete', help='ID of list to delete.')
 def delete_list(user, token, library_id, list_id):
     """Script to delete a list from target audience. 
     
@@ -26,7 +26,7 @@ def delete_list(user, token, library_id, list_id):
             }
 
     response = requests.get(url, api_params)
-    print json.dumps(response.json(), indent=4)
+    print(json.dumps(response.json(), indent=4))
 
 if __name__ == '__main__':
     delete_list()
